8df6269c91fe6703186e61ec6b60379d0ac21612
QuangBui <quangbs@selex.vn>
2024-03-15 17:27:47 +0700

    first commit

---
+++ src/main/java/vn/buiquang26/x509certificateWithBouncyCastle/X509certificateWithBouncyCastleApplication.java
@@ -0,0 +1,13 @@
+package vn.buiquang26.x509certificateWithBouncyCastle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class X509certificateWithBouncyCastleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(X509certificateWithBouncyCastleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/buiquang26/x509certificateWithBouncyCastle/X509certificateWithBouncyCastleApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.buiquang26.x509certificateWithBouncyCastle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class X509certificateWithBouncyCastleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
